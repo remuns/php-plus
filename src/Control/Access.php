a258024490d5b41c57cb1ed0d483cb3d2e0a744f
remuns <82280172+remuns@users.noreply.github.com>
2021-04-19 10:29:48 -0400

    Added Access static class
    This class allows object properties and methods to be accessed in one call from a list of string (property) or array (method) descriptions.

---
+++ src/Control/Access.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace PhpPlus\Core\Control;
+
+use PhpPlus\Core\Traits\StaticClassTrait;
+
+use TypeError;
+
+/**
+ * A static class storing functions and methods for simply and easily accessing properties and
+ * methods dynamically.
+ */
+final class Access
+{
+    use StaticClassTrait;
+
+    /**
+     * Accesses an object by either accessing properties with the names passed in or
+     * calling named methods passed in.
+     * 
+     * @param object        $val                The object to access.
+     * @param string|array  ...$accessSegments  A parameter array of values describing the access
+     *                                          to make.
+     *                                          If a property is desired, such a value should be
+     *                                          the string name of the property.
+     *                                          If a method call is desired, this should be an
+     *                                          array containing the string name of the method as
+     *                                          the first element, with the arguments included as
+     *                                          subsequent array elements.
+     * @return mixed The result of the access.
+     */
+    public static function access(object $val, string|array ...$accessSegments)
+    {
+        // Type check the segments quickly before running
+        // We want to type-check first so that no exception or error can be thrown due to an
+        // undefined property or method (or thrown due to method logic) and mask malformed input.
+        array_walk($accessSegments, 'self::typeCheckSegment');
+
+        $newVal = $val;
+        foreach ($accessSegments as $segment) {
+            // Disable the type-checking for the arrays since they have already been type-checked
+            $newVal = self::accessOnceInternal($newVal, $segment, typeCheck: false);
+        }
+
+        return $newVal;
+    }
+
+    /**
+     * Accesses an object by either accessing properties with the names passed in or
+     * calling named methods passed in.
+     * 
+     * @param object    $val            The object to access.
+     * @param array     $accessSegments An array of values describing the access to make.
+     *                                  If a property is desired, the array should include the
+     *                                  string name of the property.
+     *                                  If a method call is desired, the array should include an
+     *                                  array containing the string name of the method as the
+     *                                  first element, with the arguments included as subsequent
+     *                                  array elements.
+     * @return mixed The result of the access.
+     */
+    public static function accessArray(object $val, array $accessSegments)
+    {
+        // Type check the segments quickly before running
+        // We want to type-check first so that no exception or error can be thrown due to an
+        // undefined property or method (or thrown due to method logic) and mask malformed input.
+        array_walk($accessSegments, 'self::typeCheckSegment');
+
+        $newVal = $val;
+        foreach ($accessSegments as $segment) {
+            // Disable the type-checking for the arrays since they have already been type-checked
+            $newVal = self::accessOnceInternal($newVal, $segment, typeCheck: false);
+        }
+
+        return $newVal;
+    }
+
+    /**
+     * Accesses an object by either accessing the property with the name passed in or
+     * calling the named method passed in.
+     * 
+     * @param object        $val            The object to access.
+     * @param string|array  $accessSegment  A value describing the access to make.
+     *                                      If a property is desired, this should be the string
+     *                                      name of the property.
+     *                                      If a method call is desired, this should be an array
+     *                                      containing the string name of the method as the first
+     *                                      element, with the arguments included as subsequent
+     *                                      array elements.
+     * @return mixed The result of the access.
+     */
+    public static function accessOnce(object $val, string|array $accessSegment)
+    {
+        return self::accessOnceInternal($val, $accessSegment, typeCheck: true);
+    }
+
+    private static function accessOnceInternal(
+        object $val, string|array $accessSegment, bool $typeCheck)
+    {
+        if (is_string($accessSegment)) {
+            return $val->$accessSegment;
+        } else {
+            if ($typeCheck) { self::typeCheckFuncSegment($accessSegment); }
+
+            if (count($accessSegment) === 1) {
+                return $val->{$accessSegment[0]}();
+            } else {
+                return call_user_func_array(
+                        [$val, $accessSegment[0]], array_slice($accessSegment, 1));
+            }
+        } 
+    }
+
+    private static function typeCheckSegment(string|array $accessSegment): void
+    {
+        if (is_array($accessSegment)) {
+            if (count($accessSegment) === 0 || !is_string($accessSegment[0])) {
+                throw new TypeError(
+                    'expected an array with a string method name as its first value');
+            }
+        }
+    }
+
+    private static function typeCheckFuncSegment(array $accessSegment): void
+    {
+        if (count($accessSegment) === 0 || !is_string($accessSegment[0])) {
+            throw new TypeError(
+                'expected an array with a string method name as its first value');
+        }
+    }
+}
---
+++ tests/Control/AccessTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace PhpPlus\Core\Tests\Control
+{
+    use PhpPlus\Core\Control\Access;
+    use PhpPlus\Core\Tests\Control\AccessTest\Test1;
+    use PhpPlus\Core\Tests\Control\AccessTest\Test2;
+    use PhpPlus\Core\Tests\TestCase;
+
+    /**
+     * Tests for the Access static class.
+     * 
+     * @see Access
+     */
+    class AccessTest extends TestCase
+    {
+        /**
+         * Tests the Access::accessOnce() method.
+         * @see Access::accessOnce()
+         */
+        public function testAccessOnce()
+        {
+            $o = new Test1;
+            $this->assertFalse(Access::accessOnce($o, 'boolVal'));
+            $this->assertEquals(1, Access::accessOnce($o, ['get1']));
+            $this->assertEquals(10, Access::accessOnce($o, ['returnSum', 1, 2, 3, 4]));
+        }
+
+        /**
+         * Tests the Access::access() method.
+         * @see Access::access()
+         */
+        public function testAccess()
+        {
+            $o = new Test1;
+            $this->assertSame(4, Access::access($o, 'class', 'a'));
+            $this->assertFalse(Access::access($o, 'boolVal'));
+            $this->assertTrue(Access::access($o, ['getTest2'], 'boolVal'));
+            $this->assertSame(3, Access::access($o, ['getTest2'], ['getVal']));
+        }
+
+        /**
+         * Tests the Access::accessArray() method.
+         * @see Access::accessArray()
+         */
+        public function testAccessArray()
+        {
+            $o = new Test1;
+            $this->assertSame(4, Access::accessArray($o, ['class', 'a']));
+            $this->assertFalse(Access::accessArray($o, ['boolVal']));
+            $this->assertTrue(Access::accessArray($o, [['getTest2'], 'boolVal']));
+            $this->assertSame(3, Access::accessArray($o, [['getTest2'], ['getVal']]));
+        }
+    }
+}
+
+namespace PhpPlus\Core\Tests\Control\AccessTest
+{
+    class Test1
+    {
+        public function get1(): int { return 1; }
+        public function getTest2(): Test2 { return new Test2; }
+
+        public function returnSum(int ...$args)
+        { 
+            return array_reduce($args, fn($a, $b) => $a + $b);
+        }
+
+        public bool $boolVal = false;
+        public \stdClass $class;
+
+        public function __construct()
+        {
+            $this->class = (object)['a' => 4, 'b' => 5];
+        }
+    }
+
+    class Test2
+    {
+        public bool $boolVal = true;
+        public function getVal(): int { return 3; }
+    }
+}
