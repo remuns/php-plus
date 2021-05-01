e479afc35d05e38eb88406822ed43345b0ff0366
remuns <82280172+remuns@users.noreply.github.com>
2021-05-01 10:30:44 -0400

    Added trait for classes that have static constructors

---
+++ src/Traits/StaticConstructibleTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PhpPlus\Core\Traits;
+
+/**
+ * A trait for classes that require static set-up before interaction.
+ */
+trait StaticConstructibleTrait
+{
+    private static bool $private_isInitializedIndicator = false;
+
+    /**
+     * Sets up the class before interaction.
+     * 
+     * This method should be called immediately after the class definition so that the class
+     * can be set up. Calls to this method have no effect after the initial call.
+     * 
+     * To provide static initialization, implement the {@see self::__initStatic()} abstract method,
+     * which will be called internally by this method.
+     */
+    public static final function __constructStatic(): void
+    {
+        // Initialize the class if it has yet to be initialized
+        if (!self::$private_isInitializedIndicator) {
+            self::$private_isInitializedIndicator = true;
+            self::__initStatic();
+        } // Otherwise do nothing
+    }
+
+    /**
+     * Sets up the class before interaction.
+     * This method will be called internally by {@see self::__constructStatic()}.
+     */
+    protected static abstract function __initStatic(): void;
+}
