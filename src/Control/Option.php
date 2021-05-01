0466ea0980b07aeeee5a390352473d6a90b52b0a
remuns <82280172+remuns@users.noreply.github.com>
2021-05-01 10:32:05 -0400

    Added Option monadic class
    This class offers a type-safe alternative to null (in that it avoids collapsing wrapped null values to one value, so that Option::some(Option::none()) and Option::none() are distinct values

---
+++ src/Control/Option.php
@@ -0,0 +1,319 @@
+<?php
+
+namespace PhpPlus\Core\Control;
+
+use PhpPlus\Core\Control\Access;
+use PhpPlus\Core\Exceptions\InvalidOperationException;
+use PhpPlus\Core\Traits\StaticConstructibleTrait;
+use PhpPlus\Core\Traits\WellDefinedSelf;
+
+/**
+ * A class wrapping either a value or no value.
+ * 
+ * This provides a less ambiguous alternative to null, with additional methods for performing
+ * useful operations.
+ */
+final class Option
+{
+    use WellDefinedSelf;
+    use StaticConstructibleTrait;
+
+    /* *********************************************************** *
+     * Static constructor and properties
+     * *********************************************************** */
+    private static self $none;
+
+    protected static function __initStatic(): void
+    {
+        self::$none = new self(null, isSome: false);
+    }
+
+    /* *********************************************************** *
+     * Constructors and simple factory methods
+     * *********************************************************** */
+    private function __construct(private $value, private bool $isSome) { }
+
+    /**
+     * Returns an empty option.
+     * @return self
+     */
+    public static function none(): self { return self::$none; }
+
+    /**
+     * Returns an option wrapping the value passed in.
+     * @param mixed $value The value to wrap.
+     * @return self
+     */
+    public static function some($value): self { return new self($value, isSome: true); }
+
+    /* *********************************************************** *
+     * Basic getters
+     * *********************************************************** */
+    /**
+     * Gets whether or not this option is empty.
+     * @return bool
+     */
+    public function isNone(): bool { return !$this->isSome; }
+
+    /**
+     * Gets whether or not this option wraps a value.
+     * @return bool
+     */
+    public function isSome(): bool { return $this->isSome; }
+
+    /**
+     * Gets the value wrapped in this option, throwing an exception if the option is empty.
+     * @return mixed
+     * @throws InvalidOperationException The option was empty.
+     */
+    public function value(): mixed
+    {
+        if (!$this->isSome) {
+            throw new InvalidOperationException('attempt to unwrap an empty option');
+        }
+        return $this->value;
+    }
+
+    /**
+     * Gets the value wrapped in this option, or null if the option is empty.
+     * @return mixed
+     */
+    public function valueOrNull(): mixed { return $this->value; }
+
+    /**
+     * Determines if this option wraps a value equal to the value passed in (using a loose
+     * comparison).
+     * 
+     * @param $item The item to check for equality.
+     * 
+     * @return bool|null Whether or not the item was equal to the value wrapped in this option,
+     *                   or null if the option was empty.
+     */
+    public function has($item): ?bool { return $this->isSome ? $this->value == $item : null; }
+
+    /**
+     * Determines if this option wraps the value passed in (using a strict comparison).
+     * 
+     * @param $item The item to check for.
+     * 
+     * @return bool|null Whether or not the item was the value wrapped in this option, or null
+     *                   if the option was empty.
+     */
+    public function hasStrict($item): ?bool
+    {
+        return $this->isSome ? $this->value === $item : null;
+    }
+
+    /* *********************************************************** *
+     * Functor methods
+     * *********************************************************** */
+    /**
+     * Maps a callable over this option.
+     * @param callable $f The callable to map over the option.
+     * @return self
+     */
+    public function map(callable $f): self
+    {
+        return $this->isSome ? self::some($f($this->value)) : self::$none;
+    }
+
+    /**
+     * Maps a series of callables over this option.
+     * 
+     * This may be more efficient (not to mention readable) than making repeated calls to
+     * {@see self::map()} because this method avoids creating intermediate option values.
+     * 
+     * @param callable ...$funcs A param array of callables to map over the option.
+     * 
+     * @return self
+     */
+    public function mapAll(callable ...$funcs): self
+    {
+        if (empty($funcs)) { // Throw out empty cases without doing anything
+            return $this;
+        }
+
+        // Apply all functions in order
+        if ($this->isSome) {
+            $val = $this->value;
+            foreach ($funcs as $f) {
+                $val = $f($val);
+            }
+            return self::some($val);
+        }
+        return self::$none;
+    }
+
+    /**
+     * Allows a wrapped object to be accessed via (string) property names and (array) method
+     * descriptions.
+     * 
+     * @param string|array ...$accessSegments   A param array of access segments to use to access
+     *                                          the object.
+     * 
+     * @return self An option wrapping the result of the access, or Option::none() if the option
+     *              passed in was empty.
+     */
+    public function access(string|array ...$accessSegments): self
+    {
+        return $this->isSome ?
+                self::some(Access::access($this->value, ...$accessSegments)) :
+                self::$none;
+    }
+
+    /* *********************************************************** *
+     * Applicative methods
+     * *********************************************************** */
+    /**
+     * Applies the callable wrapped by this option to a list of arguments.
+     * @param ...$params The list of parameters to apply the callable to.
+     * @return self
+     */
+    public function apply(...$params): self
+    {
+        return $this->isSome ? self::some(($this->value)(...$params)) : self::$none;
+    }
+
+    /* *********************************************************** *
+     * Monadic methods
+     * *********************************************************** */
+    /**
+     * Binds a callable through this option.
+     * @param callable $f A callable that should take in a single value and return an option.
+     * @return self The result of applying the callable to the value wrapped in this option if it
+     *              is nonempty, or Option::none() if it is empty.
+     */
+    public function bind(callable $f): self
+    {
+        return $this->isSome ? $f($this->value) : self::$none;
+    }
+
+    /**
+     * Collapses values equivalent to false.
+     * @return self The option passed in if that option did not wrap null, or Option::none()
+     *              if it did.
+     */
+    public function collapseNull(): self
+    {
+        // No need to check isSome since none values wrap null internally and non-empty options
+        // wrapping null are getting collapsed anyways
+        return $this->value === null ? self::$none : $this;
+    }
+
+    /**
+     * Collapses values equivalent to false.
+     * @return self The option passed in if that option did not wrap a value equivalent to false,
+     *              or Option::none() if it did.
+     */
+    public function collapseFalsy(): self
+    {
+        // No need to check isSome since none values wrap null internally (which is equivalent)
+        // to false) and non-empty options wrapping falsy values are getting collapsed anyways
+        return $this->value ? $this : self::$none;
+    }
+
+    /**
+     * Allows a wrapped object to be accessed via (string) property names and (array) method
+     * descriptions, collapsing null values to Option::none().
+     * 
+     * @param string|array $accessSegments A list of access segments to use to access the object.
+     * 
+     * @return self An option wrapping the result of the access, or Option::none() if the option
+     *              passed in was empty OR if a null value was encountered.
+     */
+    public function accessNullable(string|array ...$accessSegments): self
+    {
+        if ($this->isSome) {
+            // Type-check before allowing the procedure to continue
+            array_walk($accessSegments, Access::class.'::typeCheckSegment');
+
+            // Perform the access
+            $newVal = $this->value;
+            foreach ($accessSegments as $seg) {
+                $newVal = Access::accessOnce($newVal, $seg);
+                if ($newVal === null) {
+                    // Collapse null values immediately without evaluating the rest of the
+                    // segment chain
+                    return self::$none;
+                }
+            }
+            return self::some($newVal);
+        } else {
+            return self::$none;
+        }
+    }
+
+    /**
+     * Allows a wrapped object to be accessed via (string) property names and (array) method
+     * descriptions, collapsing values equivalent to false to Option::none().
+     * 
+     * @param string|array $accessSegments A list of access segments to use to access the object.
+     * 
+     * @return self An option wrapping the result of the access, or Option::none() if the option
+     *              passed in was empty OR if a value equivalent to false was encountered.
+     */
+    public function accessFalsy(string|array ...$accessSegments): self
+    {
+        if ($this->isSome) {
+            // Type-check before allowing the procedure to continue
+            array_walk($accessSegments, Access::class.'::typeCheckSegment');
+
+            // Perform the access
+            $newVal = $this->value;
+            foreach ($accessSegments as $seg) {
+                $newVal = Access::accessOnce($newVal, $seg);
+                if (!$newVal) {
+                    // Collapse falsy values immediately without evaluating the rest of the
+                    // segment chain
+                    return self::$none;
+                }
+            }
+            return self::some($newVal);
+        } else {
+            return self::$none;
+        }
+    } 
+
+    /* *********************************************************** *
+     * Factory methods
+     * *********************************************************** */
+    /**
+     * Returns an Option wrapping the value passed in, or Option::none() if the value passed in
+     * is null.
+     * @param $val The value to wrap.
+     * @return self
+     */
+    public static function fromNullable($val): self
+    {
+        return $val === null ? self::$none : self::some($val);
+    }
+
+    /**
+     * Returns an Option wrapping the value passed in, or Option::none() if the value passed in
+     * is equivalent to false.
+     * @param $val The value to wrap.
+     * @return self
+     */
+    public static function fromFalsy($val): self
+    {
+        return (!$val) ? self::$none : self::some($val);
+    }
+
+    /**
+     * Converts the nullable option passed in to a full option (collapsing null to Option::none()).
+     * 
+     * This method can be useful when taking in default options as parameters to methods; since
+     * Option is a class and cannot be treated as a constant expression, a nullable option can be
+     * included as the default and this function can be called to ensure that null is properly
+     * collapsed into an Option instance before interaction with the parameter.
+     * 
+     * @param self|null $opt The nullable option to convert to an option.
+     * 
+     * @return self The option passed in, or Option::none() if null was passed in.
+     */
+    public static function fromNullableOption(?self $opt): self
+    {
+        return $opt === null ? self::$none : $opt;
+    }
+}
+Option::__constructStatic();
---
+++ tests/Control/OptionTest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace PhpPlus\Core\Tests\Control
+{
+
+    use PhpPlus\Core\Control\Option;
+    use PhpPlus\Core\Exceptions\InvalidOperationException;
+    use PhpPlus\Core\Tests\Control\OptionTest\TestClass;
+    use PhpPlus\Core\Tests\TestCase;
+
+    use Error;
+    use stdClass;
+
+    class OptionTest extends TestCase
+    {
+        ////////   Tests   ////////
+        public function testSimpleConstruction()
+        {
+            $this->assertTrue(Option::none()->isNone());
+            $this->assertTrue(Option::some(5)->isSome());
+        }
+
+        public function testHas()
+        {
+            // The method should use loose comparison
+            $this->assertTrue(Option::some(5)->has(5));
+            $this->assertTrue(Option::some(5)->has(5.0));
+            $this->assertFalse(Option::some(5)->has(4.0));
+            $this->assertNull(Option::none()->has(2));
+        }
+
+        public function testHasStrict()
+        {
+            // The method should use strict comparison
+            $this->assertTrue(Option::some(5)->hasStrict(5));
+            $this->assertFalse(Option::some(5)->hasStrict(5.0));
+            $this->assertFalse(Option::some(5)->hasStrict(4));
+            $this->assertNull(Option::none()->hasStrict(4));
+        }
+
+        public function testValue_success()
+        {
+            $this->assertSame(4, Option::some(4)->value());
+        }
+
+        public function testValue_error()
+        {
+            $this->expectException(InvalidOperationException::class);
+            Option::none()->value(); // Should fail with the expected exception
+        }
+
+        public function testValueOrNull()
+        {
+            $this->assertSame(4, Option::some(4)->valueOrNull());
+            $this->assertNull(Option::none()->valueOrNull());
+        }
+
+        public function testMap()
+        {
+            $squarer = fn ($x) => $x * $x;
+            $this->assertHasStrict(Option::some(4)->map($squarer), 16);
+            $this->assertIsNone(Option::none()->map($squarer));
+        }
+
+        public function testMapAll()
+        {
+            $squarer = fn ($x) => $x * $x;
+            $cuber = fn ($x) => $x * $x * $x;
+
+            $this->assertHasStrict(Option::some(2)->mapAll($squarer, $cuber), 64);
+            $this->assertIsNone(Option::none()->mapAll($squarer, $cuber));
+        }
+
+        public function testAccess()
+        {
+            $obj = new stdClass;
+            $obj->a = new TestClass;
+            $obj->a->c = 2;
+
+            $opt = Option::some($obj);
+            $this->assertHasStrict($opt->access('a', ['add1', 3]), 4);
+            $this->assertHasStrict($opt->access('a', 'c'), 2);
+            $this->assertIsNone(
+                Option::none()->access('these', 'dont', ['matter', 'in', 'this', 'case']));
+        }
+
+        public function testApply_success()
+        {
+            $func = fn ($x, $y) => $x * $y;
+            $this->assertHasStrict(Option::some($func)->apply(3, 4), 12);
+            $this->assertIsNone(Option::none()->apply(1, 2, 3));
+        }
+
+        public function testApply_typeError()
+        {
+            $this->expectException(Error::class);
+            Option::some(4)->apply(5);
+        }
+
+        public function testBind()
+        {
+            $func = fn ($x) => $x > 0 ? Option::some($x) : Option::none();
+            $this->assertHasStrict(Option::some(2)->bind($func), 2);
+            $this->assertIsNone(Option::some(-1)->bind($func));
+            $this->assertIsNone(Option::none()->bind($func));
+        }
+
+        public function testCollapseNull()
+        {
+            $this->assertHasStrict(Option::some(4)->collapseNull(), 4);
+            $this->assertIsNone(Option::some(null)->collapseNull());
+            $this->assertIsNone(Option::none()->collapseNull());
+        }
+
+        public function testCollapseFalsy()
+        {
+            $this->assertHasStrict(Option::some(4)->collapseFalsy(), 4);
+            
+            $this->assertIsNone(Option::some(0)->collapseFalsy());
+            $this->assertIsNone(Option::some(null)->collapseFalsy());
+            $this->assertIsNone(Option::some(false)->collapseFalsy());
+
+            $this->assertIsNone(Option::none()->collapseFalsy());
+        }
+
+        public function testAccessNullable()
+        {
+            $obj = new stdClass;
+            $obj->a = new TestClass;
+            $obj->b = 4;
+            $obj->c = new stdClass;
+            $obj->c->d = null;
+
+            $opt = Option::some($obj);
+            $this->assertHasStrict($opt->accessNullable('a', ['add1', 4]), 5);
+            $this->assertHasStrict($opt->accessNullable('b'), 4);
+
+            // This will hit null and stop evaluating
+            $this->assertIsNone($opt->accessNullable('c', 'd', 'e'));
+
+            $this->assertIsNone(Option::none()->accessNullable('k'));
+        }
+
+        public function testAccessFalsy()
+        {
+            $obj = new stdClass;
+            $obj->a = new TestClass;
+            $obj->b = 4;
+            $obj->c = new stdClass;
+            $obj->c->d = 0;
+
+            $opt = Option::some($obj);
+            $this->assertHasStrict($opt->accessFalsy('a', ['add1', 4]), 5);
+            $this->assertHasStrict($opt->accessFalsy('b'), 4);
+
+            // This will hit a false-equivalent value and stop evaluating
+            $this->assertIsNone($opt->accessFalsy('c', 'd', 'e'));
+
+            $this->assertIsNone(Option::none()->accessFalsy('k'));
+        }
+
+        public function testFromNullable()
+        {
+            $this->assertHasStrict(Option::fromNullable(4), 4);
+            $this->assertHasStrict(Option::fromNullable(false), false);
+            $this->assertIsNone(Option::fromNullable(null));
+        }
+
+        public function testFromFalsy()
+        {
+            $this->assertHasStrict(Option::fromFalsy(4), 4);
+            $this->assertIsNone(Option::fromFalsy(0));
+            $this->assertIsNone(Option::fromFalsy([]));
+            $this->assertIsNone(Option::fromFalsy(false));
+            $this->assertIsNone(Option::fromFalsy(null));
+        }
+
+        public function testFromNullableOption()
+        {
+            $this->assertHasStrict(Option::fromNullableOption(Option::some(40)), 40);
+            $this->assertIsNone(Option::fromNullableOption(Option::none()));
+            $this->assertIsNone(Option::fromNullableOption(null));
+        }
+        ///////////////////////////////////////
+
+        ////////   Assertion Helpers   ////////
+        protected function assertIsNone(Option $option, string $message = '')
+        {
+            $this->assertTrue($option->isNone(), $message);
+        }
+
+        protected function assertHas(Option $option, mixed $item, string $message = '')
+        {
+            $this->assertTrue($option->has($item), $message);
+        }
+
+        protected function assertHasStrict(Option $option, mixed $item, string $message = '')
+        {
+            $this->assertTrue($option->hasStrict($item), $message);
+        }
+
+        protected function assertIsSome(Option $option, string $message = '')
+        {
+            $this->assertTrue($option->isSome(), $message);
+        }
+        ///////////////////////////////////////
+    }
+}
+
+namespace PhpPlus\Core\Tests\Control\OptionTest
+{
+    class TestClass
+    {
+        public function add1($x) { return $x + 1; }
+    }
+}
