ef504eb12b3ad69b09439eddf1bd6791c27c7f07
remuns <82280172+remuns@users.noreply.github.com>
2021-05-06 16:08:09 -0400

    Added a doc comment to the InvalidOperationException class

--- src/Exceptions/InvalidOperationException.php
+++ src/Exceptions/InvalidOperationException.php
@@ -4,7 +4,10 @@ namespace PhpPlus\Core\Exceptions;
 
 use LogicException;
 
+/**
+ * An exception thrown when an invalid operation is attempted by the user.
+ */
 class InvalidOperationException extends LogicException
 {
-
+    //
 }
